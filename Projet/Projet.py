945d437d82aee40fba4344d25633e7903e75943b
Florian <florian.spire@etu.univ-cotedazur.fr>
2020-12-08 16:33:39 +0100

    Detection des mots clés dans une question

--- Projet/Projet.py
+++ Projet/Projet.py
@@ -3,18 +3,23 @@
 # Lancer ce programme pour vérifier la bonne installation
 
 import spacy
+import spacy
+from collections import Counter
+from string import punctuation
 
 spacy.prefer_gpu()
 nlp = spacy.load("fr_core_news_sm")
-entree = input()
-
 
 def token(sentence):
     # Tokeniser la phrase
     doc = nlp(sentence)
+    # Retourner le texte de chaque token
+    return [X.text for X in doc]
 
+def PoSTagger(sentence):
+    doc = nlp(sentence)
     print("Pos tagger : ") #PoS tagger
-    for token in doc:
+    for token in nlp(sentence):
         print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
               token.shape_, token.is_alpha, token.is_stop)
 
@@ -23,10 +28,21 @@ def token(sentence):
     print("Noun phrases:", [chunk.text for chunk in doc.noun_chunks])
     print("Verbs:", [token.lemma_ for token in doc if token.pos_ == "VERB"])
 
-    # Retourner le texte de chaque token
-    print("\nText de chaque token : ")
-    return [X.text for X in doc]
-
+def get_hotwords(text):
+    result = []
+    pos_tag = ['PROPN', 'ADJ', 'NOUN'] 
+    doc = nlp(text.lower()) 
+    for token in doc:
+        if(token.text in nlp.Defaults.stop_words or token.text in punctuation):
+            continue
+        if(token.pos_ in pos_tag):
+            result.append(token.text)
+    return result
 
 if __name__ == '__main__':
-    print(token(entree))
+    entree = input()
+    #print(entree)
+    #print(token(entree))
+    sortie = get_hotwords(entree)
+    print(PoSTagger(entree))
+    print(sortie)
