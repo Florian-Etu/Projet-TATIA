6379d725f714c11ef1b29f1fba42218f765b5b11
Florian <florian.spire@etu.univ-cotedazur.fr>
2020-12-23 23:11:15 +0100

    Ajout de la recherche de personne via requete dbedia
    Lorsque l'on cherche "qui est... " on obtient désormais une description correspondant à la personne recherchée.

--- Projet/Projet.py
+++ Projet/Projet.py
@@ -3,7 +3,9 @@
 # Lancer ce programme pour vérifier la bonne installation
 
 import spacy
-import spacy
+import json
+import sys
+import requests
 from spacy.matcher import Matcher
 from collections import Counter
 from string import punctuation
@@ -45,11 +47,12 @@ def get_hotwords(text):
             result.append(token.text)
     return result
 
-def type_question(question):
+def reponse(question):
+    #ANALYSE DU TYPE DE LA QUESTION (via expression régèlière en s'aidant de l'analyse NER):
     #https://spacy.io/usage/rule-based-matching
     matcher = Matcher(nlp.vocab)
-    pattern = [{"LOWER": "qui"},{"POS": "AUX"}, {"ENT_TYPE": "PER"}] #QUI + AUXILIAIRE + UN NOM DE PERSONNE = ON RECHERCHE UNE PERSONNE
-    matcher.add("Personne", None, pattern)
+    pattern = [{"LOWER": "qui"},{"POS": "AUX"}, {"ENT_TYPE": "PER", "OP": "+"}] #QUI + AUXILIAIRE + UN NOM DE PERSONNE (éventuellement prénom + nom de famille) = ON RECHERCHE UNE PERSONNE
+    matcher.add("person", None, pattern)
 
     pattern = [{"LOWER": "où"}]
     matcher.add("Lieu", None, pattern)
@@ -59,23 +62,82 @@ def type_question(question):
     matcher.add("Date", None, pattern, pattern2) 
     
     matches = matcher(question)
+
+    #Traitement selon type de la question
     for match_id, start, end in matches:
         string_id = nlp.vocab.strings[match_id]  # Get string representation
         span = question[start:end]  # The matched span
-        print(match_id, string_id, start, end, span.text)
 
-            
+        #print(match_id, string_id, start, end, span.text)
+
+        #On cherche des informations sur une personne
+        if(string_id=="person"):
+            return get_abstract([(ent.text, ent.label_) for ent in question.ents][0][0]) #On execute la fonction pour faire une requete sur le nom de la personne sur laquelle on veut des informations
+
+def query(q, epr, f='application/json'):
+    try:
+        params = {'query': q}
+        resp = requests.get(epr, params=params, headers={'Accept': f})
+        return resp.text
+    except Exception as e:
+        print(e, file=sys.stdout)
+        raise
+
+def get_abstract(requete):
+
+    json_query = json.loads(query("""prefix dbpedia: <http://dbpedia.org/resource/>
+    prefix dbpedia-owl: <http://dbpedia.org/ontology/>
+    SELECT DISTINCT ?abstract WHERE { 
+        [ rdfs:label ?name
+        ; dbpedia-owl:abstract ?abstract
+        ] .
+        FILTER langMatches(lang(?abstract),"fr")
+        VALUES ?name {""" + '"' + requete + '"' + """@en }
+        }
+        LIMIT 10""","http://dbpedia.org/sparql"))
+
+    #Dans le cas où l'on n'obtient aucun résultat en français on renvoi le résultat anglais
+    if(not json_query["results"]["bindings"]):
+        json_query = json.loads(query("""prefix dbpedia: <http://dbpedia.org/resource/>
+        prefix dbpedia-owl: <http://dbpedia.org/ontology/>
+        SELECT DISTINCT ?abstract WHERE { 
+        [ rdfs:label ?name
+        ; dbpedia-owl:abstract ?abstract
+        ] .
+        FILTER langMatches(lang(?abstract),"en")
+        VALUES ?name {""" + '"' + requete + '"' + """@en }
+        }
+        LIMIT 10""","http://dbpedia.org/sparql"))
+
+    if(not json_query["results"]["bindings"]):
+        return "Aucun résultat correspondant à votre recherche.\n"
+    
+    return json_query["results"]["bindings"][0]["abstract"]["value"]+'\n'
+
+
+
+
 if __name__ == '__main__':
     #entree = input()
-    entree = "Qui est Emmanuel Macron ?"
-    doc = nlp(entree)
+    entree = nlp("Qui est Emmanuel Macron ?")
+    print(reponse(entree))
+
+    entree = nlp("Qui est Nicolas Sarkozy ?")
+    print(reponse(entree))
+
+    entree = nlp("Qui est Zinedine Zidane ?")
+    print(reponse(entree))
+
+
+    """doc = nlp(entree)
     #print(entree)
     PoSTagger(doc)
     print(token(doc))
     sortie = get_hotwords(entree)
     print(sortie)
     NER(doc)
-    type_question(doc)
+    print(type_question(doc))
     type_question(nlp("Où est Emmanuel Macron ?"))
     type_question(nlp("En quelle année est né Emmanuel Macron ?"))
-    NER(nlp("pont de Brooklyn"))
+    print(type_question(nlp("Qui est Nicolas Sarkozy ?")))
+    NER(nlp("pont de Brooklyn"))"""
